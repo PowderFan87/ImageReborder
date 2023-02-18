7a6eb6b5f3b3d634c5769a0080850a2033e280ab
Holger Szüsz <hszuesz@gmail.com>
2023-02-18 12:28:58 +0100

    UPDATE: - widen black boxes on the sides - update overlay.jpg - add argparser to programm to pass args and show help - remove test rendering and replace with loop over passed images - reposition text and increase size

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import argparse
+import pathlib
+
 import tools
 from exif import Image
+
+
+def loadargs():
+    # define arguments with argparse for programm
+    parser = argparse.ArgumentParser(
+        prog='ImageReborder',
+        description='Programm to reborder images and put the meta data of the shot into the new image',
+        epilog='Have fun and be creative :)'
+    )
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable debug messages on console')
+    parser.add_argument('-o', '--out', action='store', type=pathlib.Path, default='out/',
+                        help='Path to out directory for new images')
+    parser.add_argument('-f', '--files', required=True, nargs='+', type=argparse.FileType('rb'),
+                        help='List of pictures to process')
+
+    return parser.parse_args()
 
 
 def printmeta(file):
     for k, v in tools.get_exif_from_file(file).items():
         if type(v) is float or type(v) is int:
-            print(f'{k:15}: {v:.2f}')
+            print(f'{k:15}: {v}')
         else:
-            print(f'{k:15}: {v:}')
+            print(f'{k:15}: {v}')
 
 
 def printallmeta(file):
@@ -20,20 +41,9 @@ def printallmeta(file):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    printmeta('input/DSC07985.jpg')
-    print('####')
-    printmeta('input/DSC08219.jpg')
-    print('####')
-    printmeta('input/DSC08002.jpg')
-    print('####')
-    printmeta('input/DSC08174.jpg')
-    print('####')
-    printmeta('input/DSC08187.jpg')
-    # printallmeta('input/DSC08187.jpg')
-    newimage = tools.reborder('input/DSC07985.jpg', tools.get_exif_from_file('input/DSC07985.jpg'))
+    # Load arguments
+    args = loadargs()
 
-    newimage.save('out/test1.jpg')
-
-    newimage = tools.reborder('input/DSC08219.jpg', tools.get_exif_from_file('input/DSC08219.jpg'))
-
-    newimage.save('out/test2.jpg')
+    # Reborder all images
+    for image in args.files:
+        tools.reborder(image, tools.get_exif_from_file(image)).save(f'{args.out}/{image.name.split("/").pop()}')
--- tools/__init__.py
+++ tools/__init__.py
@@ -21,27 +21,26 @@ def get_exif_from_file(file):
         'Contemporary': 'C'
     }
 
-    with open(file, 'rb') as img_file:
-        image = Image(img_file)
-        pilimage = PILImage.open(img_file)
+    image = Image(file)
+    pilimage = PILImage.open(file)
 
-        shutterspeed = image.get('exposure_time')
+    shutterspeed = image.get('exposure_time')
 
-        if shutterspeed < 1:
-            shutterspeed = f'1/{shutterspeed*10000:.0f}'
-        else:
-            shutterspeed = f'{shutterspeed:.0f}'
+    if shutterspeed < 1:
+        shutterspeed = f'1/{shutterspeed*10000:.0f}'
+    else:
+        shutterspeed = f'{shutterspeed:.0f}'
 
-        return {
-            'shutter_speed': shutterspeed,
-            'aperture': image.get('f_number'),
-            'focal_length': image.get('focal_length_in_35mm_film'),
-            'lens': replace_all(image.get('lens_model'), replacements),
-            'model': image.get('model'),
-            'iso': image.get('photographic_sensitivity'),
-            'width': pilimage.width,
-            'height': pilimage.height
-        }
+    return {
+        'shutter_speed': shutterspeed,
+        'aperture': image.get('f_number'),
+        'focal_length': image.get('focal_length_in_35mm_film'),
+        'lens': replace_all(image.get('lens_model'), replacements),
+        'model': image.get('model'),
+        'iso': image.get('photographic_sensitivity'),
+        'width': pilimage.width,
+        'height': pilimage.height
+    }
 
 
 def reborder(file, meta):
@@ -53,30 +52,33 @@ def reborder(file, meta):
     newimagedraw = ImageDraw.Draw(newimagebase)
 
     # right side information
-    newimagedraw.text((325, 5), f'{meta["lens"]}', (0, 0, 0), font=font)
-    newimagedraw.text((1500, 5), f'{meta["model"]}', (0, 0, 0), font=font)
+    newimagedraw.text((325, 20), f'{meta["lens"]}', (0, 0, 0), font=font)
+    newimagedraw.text((1500, 20), f'{meta["model"]}', (0, 0, 0), font=font)
 
     # left side information
-    newimagedraw.text((325, 1985), f'{meta["shutter_speed"]} sec', (0, 0, 0), font=font)
-    newimagedraw.text((650, 1985), f'f/{meta["aperture"]:.1f}', (0, 0, 0), font=font)
-    newimagedraw.text((825, 1985), f'ISO {meta["iso"]}', (0, 0, 0), font=font)
-    newimagedraw.text((1500, 1985), f'{meta["focal_length"]} mm', (0, 0, 0), font=font)
+    newimagedraw.text((325, 1965), f'{meta["shutter_speed"]} sec', (0, 0, 0), font=font)
+    newimagedraw.text((650, 1965), f'f/{meta["aperture"]:.1f}', (0, 0, 0), font=font)
+    newimagedraw.text((825, 1965), f'ISO {meta["iso"]}', (0, 0, 0), font=font)
+    newimagedraw.text((1500, 1965), f'{meta["focal_length"]} mm', (0, 0, 0), font=font)
 
-    newimagebase = newimagebase.rotate(90, expand=True)
+    # Lines background
+    linesbackground = PILImage.open('tools/overlay.jpg')
+
+    if meta['width'] >= meta['height']:
+        ratio = meta['width'] / 1880
+        linesbackground = linesbackground.rotate(270, expand=True)
+    else:
+        ratio = meta['height'] / 1920
+        newimagebase = newimagebase.rotate(90, expand=True)
 
     newimage = PILImage.new(mode='RGB', size=bgsize, color='white')
     image = PILImage.open(file)
-
-    if meta['width'] >= meta['height']:
-        ratio = meta['width'] / 1920
-    else:
-        ratio = meta['height'] / 1920
 
     image = image.resize(size=(int(meta['width'] / ratio), int(meta['height'] / ratio)))
 
     print(bgsize, image.size, calc_offset(bgsize, image.size))
 
-    newimage.paste(PILImage.open('tools/overlay.jpg'))
+    newimage.paste(linesbackground)
     newimage.paste(newimagebase, (0, 0), newimagebase)
     newimage.paste(image, calc_offset(bgsize, image.size))
 
